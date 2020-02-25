beeb5a9202837ae8c856f515d621d5665dc2a149
Alexandre Masurel <alexandre.masurel@gmail.com>
2020-02-25 21:18:35 +0100

    Add insomnia test suite + implement update and delete methods

--- app/app.ts
+++ app/app.ts
@@ -2,16 +2,16 @@
 import express = require('express');
 import url = require('url');
 
-import {Character} from "./character";
+import { Character } from "./character";
 
 // Create a new express application instance
 const app: express.Application = express();
 
 app.use(express.json());
 
-var characterList:Character[] = []
+var characterList: Character[] = []
 
-app.get('/greet', function (req, res) {
+app.get('/api/greet', function (req, res) {
   var url_parts = url.parse(req.url, true);
   var query = url_parts.query;
   if (query.name) {
@@ -22,35 +22,51 @@ app.get('/greet', function (req, res) {
 });
 
 // READ ALL
-app.get('/characters', function (req, res) {
-    res.status(200).json(characterList);
+app.get('/api/characters', function (req, res) {
+  res.status(200).json(characterList);
 });
 
 // CREATE
-app.post('/character', function (req, res) {
+app.post('/api/character', function (req, res) {
   let character = new Character(req.body.name, Number(req.body.skill));
   characterList.push(character);
   res.status(200).json(`Created character n°${character.id}`);
 });
 
 // READ
-app.get('/character/:id', function (req, res) {
-  let filteredCharacterList = characterList.filter(function(character){return "id" in character && character.id == req.params.id;})
+app.get('/api/character/:id', function (req, res) {
+  let filteredCharacterList = characterList.filter((character) => {
+    return "id" in character && character.id === req.params.id;
+  });
   if (filteredCharacterList.length > 0) {
-    res.status(200).json({"success": filteredCharacterList[0]});
+    res.status(200).json({ "success": filteredCharacterList[0] });
   } else {
-    res.status(200).json({"error": `Could not find character n°${req.params.id}`});
+    res.status(200).json({ "error": `Could not find character n°${req.params.id}` });
   }
 });
 
 // UPDATE
-app.put('/character/:id', function (req, res) {
-  res.status(200).end(`Successfully updated character n°${req.params.id}`);
+app.put('/api/character/:id', function (req, res) {
+  let charToUpdate = characterList.find((character) => {
+    return "id" in character && character.id === req.params.id;
+  });
+
+  if (!!charToUpdate) {
+    charToUpdate.name = req.body.name;
+    charToUpdate.skill = req.body.skill;
+    res.status(200).end(`Successfully updated character n°${req.params.id}`);
+  } else {
+   res.status(200).json({ "error": `Could not find character n°${req.params.id}` }); 
+  }
 });
 
 // DELETE
-app.delete('/character/:id', function (req, res) {
-  res.status(200).end(`Successfully updated character n°${req.params.id}`);
+app.delete('/api/character/:id', function (req, res) {
+  characterList.forEach( (character, index) => {
+    if(character.id === req.params.id) characterList.splice(index,1);
+  });
+  
+  res.status(200).end(`Successfully deleted character n°${req.params.id}`);
 });
 
 app.get('/', function (req, res) {
